ffc0e56812900a9e1fc2006784a3fd4f1862be07
IrinaShaforost <shaforost.ira@gmail.com>
2020-12-04 17:41:07 +0200

    account

--- js/script.js
+++ js/script.js
@@ -8,3 +8,39 @@ $('body').on('click', '.password-control', function () {
 	}
 	return false;
 });
+let price = document.querySelector('.price-one').textContent; // или просто задать price числом
+console.log(price)
+let num = document.querySelector('#num').value;
+
+let calcPrice = () => {
+	return price * num;
+}
+
+let start = () => {
+	num = 1
+	document.querySelector('#num').value = num;
+	document.querySelector('.out-price').innerHTML = calcPrice();
+}
+start();
+
+let change = () => {
+	num = document.querySelector('#num').value;
+	document.querySelector('.out-price').innerHTML = calcPrice();
+}
+
+let plus = () => {
+	num++;
+	document.querySelector('#num').value = num;
+	document.querySelector('.out-price').innerHTML = calcPrice();
+}
+let minus = () => {
+	if (num != 0) {
+		num--;
+		document.querySelector('#num').value = num;
+		document.querySelector('.out-price').innerHTML = calcPrice();
+	}
+}
+
+document.querySelector('.product-plus').addEventListener('click', plus);
+document.querySelector('.product-minus').addEventListener('click', minus);
+document.querySelector('#num').addEventListener('change', change);
